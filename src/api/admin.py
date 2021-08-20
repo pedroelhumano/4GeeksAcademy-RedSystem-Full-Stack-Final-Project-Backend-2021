3d5b7fdc179f831bb13a868b8f89773bb67f2650
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-08-20 01:46:12 +0000

    base de datos arriba okey

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
-  
+
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Contrato, StatusOrden, OrdenTrabajo, DetalleOrdenTrabajo, Acreditacion, UserOrden
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,12 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Contrato, db.session))
+    admin.add_view(ModelView(StatusOrden, db.session))
+    admin.add_view(ModelView(OrdenTrabajo, db.session))
+    admin.add_view(ModelView(DetalleOrdenTrabajo, db.session))
+    admin.add_view(ModelView(Acreditacion, db.session))
+    admin.add_view(ModelView(UserOrden, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
