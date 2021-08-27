b4909ba636d6051117bd1895263fd3d75985fbe3
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-27 01:50:47 +0000

    Se crean los token, se encripta la contraseña y se crearon algunos endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -77,7 +77,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=False, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.Text, unique=False, nullable=False)
     rut = db.Column(db.String(12), unique=False, nullable=False)
     name = db.Column(db.String(100), unique=False, nullable=False)
     lastname = db.Column(db.String(100), unique=False, nullable=False)
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User #Y todo lo que se necesite
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash       ## Nos permite manejar tokens por authentication (usuarios)    
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity   #from models import Person
+import datetime
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,82 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/hash', methods=['POST', 'GET'])
+def handle_hash():
+    
+    #Deberia ser post
+    #email = request.json.get("email", None)
+    #identity=email
+
+    # Genera token
+    expiracion = datetime.timedelta(days=1) #  Genera una fecha en UTC
+    access_token = create_access_token(identity='mortega@4geeks.co', expires_delta=expiracion)
+
+    # Crear password
+    password = '123456'
+    #password = request.json.get("password", None)
+    password=generate_password_hash(password, method='sha256')
+
+    response_token = {
+        "users": "Manu",
+        "token": access_token,
+        "password": password
+    }
+
+    return jsonify(response_token), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+ if request.method == 'POST':
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    rut = request.json.get("rut", None)
+    name = request.json.get("name", None)
+    lastname = request.json.get("lastname", None)
+    contact = request.json.get("contact", None)
+    register = request.json.get("register", None)
+    perfil = request.json.get("perfil", None)
+    fecha_nacimiento = request.json.get("fecha_nacimiento", None)
+    
+    if not email:
+        return "Email required", 401
+    password = request.json.get("password", None)
+    if not password:
+        return "Password required", 401
+
+    email_query = User.query.filter_by(email=email).first()
+    if email_query:
+        return "This email has been already taken", 401
+    
+    user = User()
+    user.email = email
+    # user.is_active= True
+    # Ahora se encripta la contrasena
+    hashed_password=generate_password_hash(password, method='sha256')
+    user.password = hashed_password
+    user.rut = rut
+    user.name = name
+    user.lastname = lastname
+    user.contact = contact
+    user.register = register
+    user.perfil = perfil
+    user.fecha_nacimiento = fecha_nacimiento
+
+    print(user)
+    db.session.add(user)
+    db.session.commit()
+
+    response = {
+        "msg": "Added successfully",
+        "pasword": hashed_password
+    }
+    return jsonify(response), 201
+
+    return jsonify(response_body), 200
+
+"""from werkzeug.security import check_password_hash
+check_password_hash('pbkdf2:sha1:1000$tYqN0VeL$2ee2568465fa30c1e6680196f8bb9eb0d2ca072d', 'foobar')
+True
+check_password_hash('pbkdf2:sha1:1000$XHj5nlLU$bb9a81bc54e7d6e11d9ab212cd143e768ea6225d', 'foobar')
+True """
