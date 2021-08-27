431b15953e5c462706d76be1c3788f1d03908322
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-08-27 18:19:34 +0000

    se actualiza el modelo en orden de trabajo le faltaba un campo, ademas se agregan las rutas para obtener ordenes de trabajo base contrato y ordenes de trabajo base id en espeifico

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,8 @@
-  
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Contrato
+from api.models import db, User, Contrato, OrdenTrabajo
 from api.utils import generate_sitemap, APIException
 
 
@@ -19,6 +18,8 @@ api = Blueprint('api', __name__)
 
 #     return jsonify(response_body), 200
 
+
+#Ruta para lista de usuarios
 @api.route('/lista_users', methods=['GET'])
 def get_users():
     query_user = User.query.all()
@@ -30,11 +31,13 @@ def get_users():
     }
     return jsonify(response_body), 200
 
+#Ruta para datos especificos de un usuario
 @api.route('/lista_users/<int:id>', methods=['GET'])
 def get_user(id = None):
     query_user = User.query.filter_by(id=id).first()
     return jsonify(query_user.datosusuario()), 200
 
+#Ruta para la lista de los contratos
 @api.route('/lista_contratos', methods=['GET'])
 def get_contratos():
     query_contratos = Contrato.query.all()
@@ -45,7 +48,21 @@ def get_contratos():
     }
     return jsonify(response_body), 200
 
+#Rutas para datos especificos de un contrato
 @api.route('/lista_contratos/<int:id>', methods=['GET'])
 def get_contrato(id = None):
     query_contratos = Contrato.query.filter_by(id=id).first()
     return jsonify(query_contratos.datoscontrato()), 200
+
+#Rutas para lista de ordenes de trabajo basado en un contrato
+@api.route('/order_trabajo/<int:id_contrato>', methods=['GET'])
+def get_order_trabajo_by_contrato(id_contrato = None):
+    query_order_trabajo = OrdenTrabajo.query.filter_by(id_contrato = id_contrato)
+    query_order_trabajo = list(map(lambda x: x.listaorden(), query_order_trabajo))
+    return jsonify(query_order_trabajo), 200
+
+#Lista para una orden de trabajo en especifica y sus datos
+@api.route('/order/<int:id>', methods=['GET'])
+def get_order_trabajo(id = None):
+    query_order_trabajo = OrdenTrabajo.query.filter_by(id = id).first()
+    return jsonify(query_order_trabajo.datoscorden()), 200
