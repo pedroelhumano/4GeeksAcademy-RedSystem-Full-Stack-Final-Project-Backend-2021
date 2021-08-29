a69a646841c9208cd1395a9befac4958d8dbc228
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-29 18:35:40 +0000

    Creadas las rutas de POST y DELETE de Orden de Trabajo

--- migrations/versions/0e159ea0677f_.py
+++ migrations/versions/0e159ea0677f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 76c2d687d7bb
+Revision ID: 0e159ea0677f
 Revises: 
-Create Date: 2021-08-29 00:17:41.684619
+Create Date: 2021-08-29 18:08:46.849554
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '76c2d687d7bb'
+revision = '0e159ea0677f'
 down_revision = None
 branch_labels = None
 depends_on = None
